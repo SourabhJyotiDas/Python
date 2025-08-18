eba9e7420064fcead08ad9e08226707f4c36c209
Sourabh Jyoti Das <95738675+SourabhJyotiDas@users.noreply.github.com>
2025-08-19 00:25:19 +0530

    fix

---
+++ 01-basics/StringMethods.py
@@ -0,0 +1,26 @@
+
+
+fruits = "Apple, Banana, Mango, Orange"
+# print(fruits)
+
+convertToArray = fruits.split(",  ")
+# print(convertToArray)
+
+flavour = "Chocolate"
+quantity = 3
+output_str = "I ordered {} cups of {} Tea"
+
+res = output_str.format(quantity,flavour)
+
+# print(res) 
+
+Arr = ["Mango","Banana","Apple"]
+res2 = ", ".join(Arr)
+
+# print(res2)
+
+myStr  = "Hello World"
+lengthRes = len(myStr)
+
+# print(lengthRes)
+
