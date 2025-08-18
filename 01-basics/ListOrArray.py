0a14629c85f3f90a87fa229e82115820442d4408
Sourabh Jyoti Das <95738675+SourabhJyotiDas@users.noreply.github.com>
2025-08-19 00:54:58 +0530

    19-08-2025

---
+++ 01-basics/ListOrArray.py
@@ -0,0 +1,31 @@
+
+colors = ["Green","Orange","Black","White", "Yellow"]
+
+res =  colors[0] = "Red"
+
+print(colors)
+
+# for ele in colors: 
+    # print(ele)
+    
+# print(len(colors))
+
+
+# if "Black" in colors:
+#     print("Find output")
+    
+# print(colors.pop()) last index
+
+# print(colors.remove("Black")) 
+
+# print(colors)
+
+# print(colors.insert(0, "Primary"))
+
+# print(colors.append("Secondary"))
+
+
+
+print(colors)
+
+print ("Finish")
