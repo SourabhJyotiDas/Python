3e4d7f0488109ce96f093821e4f41396b22d1582
Sourabh Jyoti Das <95738675+SourabhJyotiDas@users.noreply.github.com>
2025-08-19 21:13:58 +0530

    19-08-2025

---
+++ 01-basics/Distionary.py
@@ -0,0 +1,28 @@
+std1 = {
+    "Name": "John Doe",
+    "Age": 26,
+    "rollno": 69
+}
+
+# print(std1["Name"])
+
+# for ele in std1:
+#     print(ele)
+
+# del std1["rollno"]   # just delete, no return value
+# print(std1)
+
+treeObj = {
+    "colors":{
+        "Blue":"Primary",
+        "Red":"Danger",
+        "Green":"Success"
+        },
+    "students":{
+        "Sourabh": "1st",
+        "Gautam":"2nd",
+        "kapil":"3rd"
+    }
+}
+
+print(treeObj["colors"]["Blue"])
