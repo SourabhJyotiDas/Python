4d1455867924f81b95e05e08eda98c256e8f26c7
Sourabh Jyoti Das <95738675+SourabhJyotiDas@users.noreply.github.com>
2025-08-20 00:39:26 +0530

    Ezzzz

---
+++ 01-basics/ifElse.py
@@ -0,0 +1,16 @@
+age = 19
+
+if age == 18 and type(age) is int:
+    print("age is 18 and is an integer")
+
+if age > 18:
+    print("You are eligible")
+elif age == 18:
+    print("You just became eligible")
+else:
+    print("You are NOT eligible")
+
+print("Finish")
+
+if age >18 and age<20 :
+    print("You are 19")
