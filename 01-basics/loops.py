0dbc8b70bdacc20f211b593f9898888b669c2547
Sourabh Jyoti Das <95738675+SourabhJyotiDas@users.noreply.github.com>
2025-08-25 00:17:23 +0530

    25-08-2025

--- 01-basics/loops.py
+++ 01-basics/loops.py
@@ -1,2 +1,13 @@
 
 
+colorArr = ["Green","Yellow","Black","Orange","Pink","White"]
+
+newArr = []
+index = 0
+
+for color in  colorArr :
+    index = index +1
+    newArr.append(f"{color} is in the {index}")
+
+print(newArr)
+    
