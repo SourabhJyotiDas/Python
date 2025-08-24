343d613a0daf906827958d1163ab5e7e8bf416a7
Sourabh Jyoti Das <95738675+SourabhJyotiDas@users.noreply.github.com>
2025-08-25 00:43:05 +0530

    25-08-2025

---
+++ 01-basics/Functions.py
@@ -0,0 +1,16 @@
+
+
+def suqareOfNum (number):
+    output = number ** 2
+    return output
+    
+# result = suqareOfNum(4)
+# print(result)
+
+
+def addTwoValue (a,b):
+    return a + b
+
+# result = addTwoValue(6,6)        
+# print(result)
+
